e2e65eb04cc1524b1b2038e8ec07252ef41c0e06
AbeGe <gyhanonline@hotmail.com>
2015-02-20 00:33:27 +0000

    refactor code
    1. marge code on_old_data

--- AgTDDealDataGeter.py
+++ AgTDDealDataGeter.py
@@ -10,23 +10,20 @@ import datetime
 import time
 
 
-# TODO: get_data and get_old_data should be marge to one function. The 'text' adjust should be use a delegate to pass in.
-def get_data(fo,md,soup):
+def get_data(fo,md,soup, direction, old_data_handler):
     text = [ [md] + [modify_data(tCol.get_text()) for tCol in tRow.find_all('td')] for tRow in soup.find_all('tr')]
+    if old_data_handler != None:
+        old_data_handler(text, direction)
     content = '\n'.join(['|'.join([col for col in row]) for row in text if row[1].startswith('Ag(T+D')])
     fo.write('\n')
     fo.write(content)
 #=============End function============
 
-def get_old_data(fo,md,soup,direction):
-    text = [ [md] + [tCol.get_text() for tCol in tRow.find_all('td')] for tRow in soup.find_all('tr')]
-    for row in text:
-        row.insert(12,modify_data(direction))
-    content = '\n'.join(['|'.join([col for col in row]) for row in text if row[1].startswith('Ag(T+D')])
-    fo.write('\n')
-    fo.write(content)
-#=============End function============
-
+def on_old_data(deal_data, direction):
+    for row in deal_data:
+        row.insert(12,modify_data(direction))  #bug here, only the Ag deal direction was added. 
+#============End function==============
+        
 def modify_data(value):
     value = value.replace('\n','').replace('\xa0','').replace('\u3000','')
     value = value.replace('多</','多支付空')
@@ -41,7 +38,7 @@ def modify_data(value):
 
 preUrl = 'http://www.sge.com.cn/xqzx/mrxq/'
 fo = open('AgData2.txt','w',encoding='utf-8')
-for i in range(1,113,1):  #113
+for i in range(1, 113,1):  #113
   page = urllib.request.urlopen(preUrl + 'index' + ("_"+str(i) if i>1 else "") + '.shtml').read().decode('UTF8')
   
   #'http://www.sge.com.cn/xqzx/mrxq/524349.shtml'
@@ -57,9 +54,9 @@ for i in range(1,113,1):  #113
         direction=''
     print(md,end='\n\n')
     if datetime.datetime.strptime(md, '%Y-%m-%d')< datetime.datetime(2014,9,5):
-        get_old_data(fo,md,soup,direction)
+        get_data(fo,md,soup,direction,on_old_data)
     else:
-        get_data(fo,md,soup)
+        get_data(fo,md,soup, None, None)
 fo.close()
 print('end')
 
